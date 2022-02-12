6b264c9462afa7bbfc36f306905669124d7468c4
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2022-02-12 16:31:05 -0300

    Remove unused code

--- tj_scraper/export.py
+++ tj_scraper/export.py
@@ -1,8 +1,7 @@
 """Deals with export formats."""
 from collections.abc import Collection
-from functools import partial
 from pathlib import Path
-from typing import Callable, Union
+from typing import Union
 
 import openpyxl
 
@@ -52,12 +51,6 @@ def flatten(process: Process) -> dict[str, str]:
         result["AudienciaDescrição"] = str(audiencia["descr"])
         result["AudienciaCódigoResultado"] = str(audiencia["codResultAud"])
 
-    if "dividaAtivas" in process:
-        divida: list[Object]
-        for i, divida in enumerate(process.pop("dividaAtivas"), start=1):  # type: ignore
-            result[f"DividaAtiva{i}AnoExerc"] = str(divida.pop("anoExerc"))  # type: ignore
-            assert not divida
-
     if "mandado" in process:
         mandado: Object
         for i, mandado in enumerate(process.pop("mandado"), start=1):  # type: ignore
@@ -92,65 +85,8 @@ def flatten(process: Process) -> dict[str, str]:
     return result
 
 
-def prepare_to_export(data: Collection[Process]) -> list[Object]:
+def prepare_to_export(raw_data: Collection[Process]) -> list[Object]:
     """Rearranges data to be in a format easy to iter and export."""
-
-    def merge(
-        values: list[dict[str, str]],
-        operation: Callable[[Object, Object], Object],
-        condition: Callable[[Object], bool],
-    ) -> dict[str, str]:
-        merged: Object = {}
-        for value in values:
-            print(f"{value=}")
-            if condition(value):
-                merged = operation(merged, value)
-        return merged
-
-    def nomes_together(obj, new):
-        print(f"{obj=} + {new=}")
-        autores = [
-            name
-            for name in (obj.get("autores", ""), new["nome"])
-            if name and new["descPers"] == "Autor"
-        ]
-        reus = [
-            name
-            for name in (obj.get("réus", ""), new["nome"])
-            if name and new["descPers"] == "Réu"
-        ]
-        return {
-            "autores": ", ".join(autores),
-            "réus": ", ".join(reus),
-        }
-
-    special_cases = {
-        "personagens": partial(
-            merge,
-            operation=nomes_together,
-            condition=lambda obj: obj["descPers"] in ["Autor", "Réu"],
-        ),
-    }
-
-    def value_or_special(k: str, value: ProcessField) -> list[tuple[str, str]]:
-        if k in special_cases:
-            print(f"{k} is special :3")
-            replaced_fields = list(
-                (k_, v_) for k_, v_ in special_cases[k](value).items()
-            )
-            print(f"{replaced_fields=}")
-            return replaced_fields
-        return [(k, str(value))]
-
-    return [
-        dict(value for k, v in item.items() for value in value_or_special(k, v))
-        for item in data
-    ]
-
-
-def export_to_xlsx(raw_data: Collection[Process], path: Path) -> None:
-    """Exports data into a XLSX file."""
-    # data = prepare_to_export(raw_data)
     raw_data = select_fields(
         raw_data,
         [
@@ -165,7 +101,12 @@ def export_to_xlsx(raw_data: Collection[Process], path: Path) -> None:
             "ultMovimentoProc",
         ],
     )
-    data = [flatten(item) for item in raw_data]
+    return [flatten(item) for item in raw_data]
+
+
+def export_to_xlsx(raw_data: Collection[Process], path: Path) -> None:
+    """Exports data into a XLSX file."""
+    data = prepare_to_export(raw_data)
 
     book = openpyxl.Workbook()
 
